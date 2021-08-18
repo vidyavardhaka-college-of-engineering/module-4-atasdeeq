2e0ccd3423803c929673afae9176b5ca96eb4e92
atasdeeq <84765801+atasdeeq@users.noreply.github.com>
2021-08-18 20:32:53 +0530

    updated

--- Program-8.c
+++ Program-8.c
@@ -9,12 +9,10 @@ int main()
    int arr1[10];
    int i,n, sum = 0;
    int *pt;
-   printf(" Input the number of elements to store in the array (max 10) : ");
+   printf(" Input the number of elements to store in the array (max 10) and enter the elements: ");
    scanf("%d",&n);
-   printf(" Input %d number of elements in the array : \n",n);
    for(i=0;i<n;i++)
       {
-	  printf(" element - %d : ",i+1);
 	  scanf("%d",&arr1[i]);
 	  } 	
   pt = arr1; // pt store the base address of array arr1
